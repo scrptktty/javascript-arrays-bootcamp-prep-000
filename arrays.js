7f872acb3ecda9770d525106ca7aff2a922f9f54
Kachel Wittig <kjwittig@gmail.com>
2018-02-06 09:16:37 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,37 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function accessElementInArray(a, b) { return a[b] };
+
+function addElementToBeginningOfArray(a, b) { return [b, ...a] }
+
+function destructivelyAddElementToBeginningOfArray(a, b) {
+ a.unshift(b)
+ return a
+}
+
+function addElementToEndOfArray(a, b) { return [...a, b] }
+
+function destructivelyAddElementToEndOfArray(a, b) {
+ a.push(b)
+ return a
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(a, b) {
+  a.shift(b)
+  return a
+}
+
+function removeElementFromBeginningOfArray(a, b) {
+  a.slice(b, (a.length-1))
+  return a
+}
+
+function destructivelyRemoveElementFromEndOfArray(a, b) {
+  a.pop(b)
+  return a
+}
+
+function removeElementFromEndOfArray(a) {
+  a.length = (a.length - 1)
+  return a
+}
